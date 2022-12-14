33c8286f0c2e85e740e6a57b3b4ef70bea3afd7c
Xushkiyor <48253018+Xushkiyor@users.noreply.github.com>
2022-12-14 06:21:37 +0500

    Initial Commit

---
+++ Weather-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by Nosirov Xushkiyor Shavkatbek o'g'li on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
